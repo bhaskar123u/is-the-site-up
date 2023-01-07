8f75e6a038d167fcab3b505e9c6e4419632f03b2
Bhaskar Sharan <bhaskar123u@gmail.com>
2023-01-07 20:25:58 +0530

    initial commit

---
+++ src/test/java/io/bhaskar_sharan_1/isthesiteup/IsTheSiteUpApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bhaskar_sharan_1.isthesiteup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsTheSiteUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
